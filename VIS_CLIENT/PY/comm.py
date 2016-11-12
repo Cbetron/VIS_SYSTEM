eecd99a46724174a1b55c40346651403b1a05fab
D3D53C <marc.steinebrunner@gmail.com>
2016-11-12 17:10:08 +0100

    Add files via upload

---
+++ VIS_CLIENT/PY/comm.py
@@ -0,0 +1,28 @@
+
+class Comm(self):
+    import socket
+
+        def client(host, port, message):
+
+                s = socket.socket()
+                s.connect((host, port))
+                while True:
+                    _message = message
+                    s.send(_message.encode('utf-8'))
+                    data = s.recv(1024).decode('utf-8')
+                    return data
+                s.close()
+
+        def Server(host, port):
+            s = socket.socket()
+            s.bind((host,port))
+            s.listen(1)
+            c, addr = s.accept()
+            while True:
+                data = c.recv(1024).decode('utf-8')
+                if not data:
+                    break
+                else
+                    return data
+                c.send(data.encode('utf-8'))
+            c.close()
